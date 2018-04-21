0a4221e5d090b55619f4937dc4a78436429900a8
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-04-20 21:28:46 -0400

    Finished register.php.

--- register.php
+++ register.php
@@ -1,18 +1,64 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>Welcome to Project Spotify!</title>
     <link href="main.css" rel="stylesheet" type="text/css" />
-  </head>
-  <body>
-  	<div class="inputContainer">
-  		<form action="register.php" class="loginForm" method="POST"></form>
-  		<h2>Login to your Account.</h2>
-  		<input type="text" class="loginUsername" name="loginUsername" placeholder="User Name" required>
-  		<input type="password" class="loginPassword" name="loginPassword" required>
-  	</div>
+</head>
+
+<body>
+    <div class="inputContainer">
+        <form action="register.php" id="loginForm" method="POST">
+            <h2>Login to your account</h2>
+            <p>
+                <label for="loginUsername">Username</label>
+                <input type="text" id="loginUsername" name="loginUsername" placeholder="User Name" required>
+            </p>
+            <p>
+                <label for="loginPassword">Password</label>
+                <input type="password" id="loginPassword" name="loginPassword" placeholder="Enter password" required>
+            </p>
+            <button type="submit" name="loginButton">Log In</button>
+        </form>
+		
+		<form action="register.php" id="registerForm" method="POST">
+            <h2>Create your free account</h2>
+            <p>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="User Name" required>
+            </p>
+            <p>
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
+            </p>
+            <p>
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" required>
+            </p>
+            <p>
+                <label for="email2">Confirm email</label>
+                <input type="email" id="email2" name="email2" placeholder="Confirm email" required>
+            </p>
+
+            <p>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter password" required>
+            </p>
+             <p>
+                <label for="password2">Confirm password</label>
+                <input type="password" id="password2" name="password2" placeholder="Enter password" required>
+            </p>
+            <button type="submit" name="registerButton">SIGN UP</button>
+        </form>
+
+    </div>
     <script src="script.js"></script>
-  </body>
+</body>
+
 </html>
