cdfe2853cf47827c5ffe2b8e254c9e0e303e573a
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-04-21 15:56:57 -0400

    Added class Constants in Constants.php

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -13,6 +13,21 @@
 			$this->validateLastName($ln);
 			$this->validateEmails($em, $em2);
 			$this->validatePasswords($pw, $pw2);
+
+			if(empty($this->errorArray) == true) {
+				//Insert into db
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		// Checks if error exists in errorArray
+		public function getError() {
+			if(!in_array($error, $this->errorArray)) {
+				$error = "";
+			}
+			return "<span class='errorMessage'>$error</span>";
 		}
 
 		private function validateUsername($un) {
--- register.php
+++ register.php
@@ -37,18 +37,23 @@
 		<form action="register.php" id="registerForm" method="POST">
             <h2>Create your free account</h2>
             <p>
+								<?php echo $account->getError("Your username must be between 5 and 25 characters"); ?>
                 <label for="username">Username</label>
                 <input type="text" id="username" name="username" placeholder="User Name" required>
             </p>
             <p>
+								<?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
                 <label for="firstName">First Name</label>
                 <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
             </p>
             <p>
+								<?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
                 <label for="lastName">Last Name</label>
                 <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
             </p>
             <p>
+							<?php echo $account->getError("Your emails don't match"); ?>
+							<?php echo $account->getError("Email is invalid"); ?>
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" placeholder="Email" required>
             </p>
