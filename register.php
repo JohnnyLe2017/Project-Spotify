85ea40ce8eb06f5d8fd23b44a3c9b23f5dbc131c
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-04-22 15:38:56 -0400

    Removed checkmark.png

--- register.php
+++ register.php
@@ -22,7 +22,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>Welcome to Project Spotify!</title>
-		<link href="css/register.css" rel="stylesheet" type="text/css" />
+
+    <!--Font Awesome 5-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+
+	<link href="css/register.css" rel="stylesheet" type="text/css" />
 		<!--jQuery-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 </head>
