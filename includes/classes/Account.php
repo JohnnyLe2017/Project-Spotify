5292fe7afff19bca975b4d55e9d33a7cd3d93c1f
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-04-22 09:21:07 -0400

    Started styling with css

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -79,6 +79,13 @@ class Account {
 			array_push($this->errorArray, Constants::$firstNameCharacters);
 			return;
 		}
+
+		$checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
+			if(mysqli_num_rows($checkUsernameQuery) != 0) {
+				array_push($this->errorArray, Constants::$usernameTaken);
+				return;
+			}
+
 	}
 
 	private function validateLastName($ln) {
