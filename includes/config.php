273347f0e5ac7c02ede429baf89613ebe73b7098
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-04-21 20:51:47 -0400

    Commit

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,8 @@
 <?php
 ob_start();
+
 $timezone = date_default_timezone_set("America/New_York");
-$con = mysqli_connect("localhost", "root", "root", project_spotify);
+$con      = mysqli_connect("localhost", "root", "root", project_spotify);
 if(mysqli_connect_error()) {
   echo "Failed to connect: " . mysqli_connect_error();
 }
