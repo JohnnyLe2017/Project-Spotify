949af476100be2ed46769c6341aa76ee2f9d392f
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-05-12 19:28:46 -0400

    Commits

--- assets/sql files/artists.sql
+++ assets/sql files/artists.sql
@@ -1,9 +1,25 @@
 
 /* Create Artists Table */
-CREATE TABLE `project_spotify`.`artists` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `project_spotify`.`artists` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
 
 /* INSERT INTO Statements */
-INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'Leandro Da Silva')
-INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'CID')
-INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'Gianni Blu')
-INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'Anto & Lyle M')
+INSERT INTO `artists` (`id`, `name`)
+VALUES
+  (NULL, 'Leandro Da Silva')
+
+INSERT INTO `artists` (`id`, `name`)
+VALUES
+  (NULL, 'CID')
+
+INSERT INTO `artists` (`id`, `name`)
+VALUES
+  (NULL, 'Gianni Blu')
+
+INSERT INTO `artists` (`id`, `name`)
+VALUES
+  (NULL, 'Anto & Lyle M')
--- assets/sql files/genres.sql
+++ assets/sql files/genres.sql
@@ -1,6 +1,27 @@
 
 /* Create Genres Table */
-CREATE TABLE `project_spotify`.`genres` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `project_spotify`.`genres` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
 
 /* INSERT INTO Statements */
-INSERT INTO `genres` (`id`, `name`) VALUES (NULL, 'House'), (NULL, 'Pop'), (NULL, 'Rock'), (NULL, 'Hip-Hop'), (NULL, 'Rap'), (NULL, 'Classical'), (NULL, 'Jazz'), (NULL, 'Electronic Dance'), (NULL, 'Popular'), (NULL, 'Punk Rock'), (NULL, 'Heavy Metal'), (NULL, 'Techno'), (NULL, 'Disco'), (NULL, 'Dance'), (NULL, 'Indie Rock');
+INSERT INTO `genres` (`id`, `name`)
+VALUES
+  (NULL, 'House'),
+  (NULL, 'Pop'),
+  (NULL, 'Rock'),
+  (NULL, 'Hip-Hop'),
+  (NULL, 'Rap'),
+  (NULL, 'Classical'),
+  (NULL, 'Jazz'),
+  (NULL, 'Electronic Dance'),
+  (NULL, 'Popular'),
+  (NULL, 'Punk Rock'),
+  (NULL, 'Heavy Metal'),
+  (NULL, 'Techno'),
+  (NULL, 'Disco'),
+  (NULL, 'Dance'),
+  (NULL, 'Indie Rock');
