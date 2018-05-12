380a1da88a73571a820a573fba459f6787068226
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-05-12 18:40:23 -0400

    Updates

--- assets/sql files/artists.sql
+++ assets/sql files/artists.sql
@@ -1,2 +1,7 @@
 
 CREATE TABLE `project_spotify`.`artists` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'Leandro Da Silva')
+INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'CID')
+INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'Gianni Blu')
+INSERT INTO `artists` (`id`, `name`) VALUES (NULL, 'Anto & Lyle M')
--- assets/sql files/genres.sql
+++ assets/sql files/genres.sql
@@ -1,2 +1,4 @@
 
 CREATE TABLE `project_spotify`.`genres` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+INSERT INTO `genres` (`id`, `name`) VALUES (NULL, 'House'), (NULL, 'Pop'), (NULL, 'Rock'), (NULL, 'Hip-Hop'), (NULL, 'Rap'), (NULL, 'Classical'), (NULL, 'Jazz'), (NULL, 'Electronic Dance'), (NULL, 'Popular'), (NULL, 'Punk Rock'), (NULL, 'Heavy Metal'), (NULL, 'Techno'), (NULL, 'Disco'), (NULL, 'Dance'), (NULL, 'Indie Rock');
