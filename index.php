c3262563dc1b2e6f90c21582d2bb36585a102f4d
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-05-12 19:50:30 -0400

    Cleaned up Navigation Area

--- index.php
+++ index.php
@@ -22,45 +22,7 @@ else {
 	<div id="mainContainer">
 
 		<div id="topContainer">
-
-			<div id="navBarContainer">
-				<nav class="navBar">
-
-					<a href="index.php" class="logo">
-						<img src="assets/images/icons/logo.png">
-					</a>
-
-
-					<div class="group">
-
-						<div class="navItem">
-							<a href="search.php" class="navItemLink">Search
-								<img src="assets/images/icons/search.png" class="icon" alt="Search">
-							</a>
-						</div>
-
-					</div>
-
-					<div class="group">
-						<div class="navItem">
-							<a href="browse.php" class="navItemLink">Browse</a>
-						</div>
-
-						<div class="navItem">
-							<a href="yourMusic.php" class="navItemLink">Your Music</a>
-						</div>
-
-						<div class="navItem">
-							<a href="profile.php" class="navItemLink">Johnny Le</a>
-						</div>
-					</div>
-
-
-
-
-				</nav>
-			</div>
-
+			<?php include("includes/navBarContainer.php");?>
 		</div>
 
 
