5e6fd8e5f0b3b86b91ec930fa9e09dc89d713d23
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-05-12 18:45:50 -0400

    Commits

--- assets/sql files/albums.sql
+++ assets/sql files/albums.sql
@@ -1,2 +1,5 @@
 
 CREATE TABLE `project_spotify`.`albums` ( `id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NOT NULL , `artist` INT NOT NULL , `genre` INT NOT NULL , `artworkPath` VARCHAR(500) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+/* Single INSERT INTO Statement */
+INSERT INTO `albums` (`id`, `title`, `artist`, `genre`, `artworkPath`) VALUES (NULL, 'So Excited (Calippo Remix)', '1', '1', 'assets/images/artwork/selected01.JPG')
