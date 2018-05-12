4a0f8c9d2e09437cb025c181912cedcb445889bc
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-05-12 19:30:24 -0400

    Commits

--- assets/sql files/Songs.sql
+++ assets/sql files/Songs.sql
@@ -1,6 +1,18 @@
 
 /* Create Songs Table */
-CREATE TABLE `project_spotify`.`Songs` ( `id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NOT NULL , `artist` INT NOT NULL , `album` INT NOT NULL , `genre` INT NOT NULL , `duration` VARCHAR(8) NOT NULL , `path` VARCHAR(500) NOT NULL , `albumOrder` INT NOT NULL , `plays` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `project_spotify`.`Songs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `artist` INT NOT NULL,
+  `album` INT NOT NULL,
+  `genre` INT NOT NULL,
+  `duration` VARCHAR(8) NOT NULL,
+  `path` VARCHAR(500) NOT NULL,
+  `albumOrder` INT NOT NULL,
+  `plays` INT NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
 
 /* Song 1 */
 INSERT INTO `songs` (
