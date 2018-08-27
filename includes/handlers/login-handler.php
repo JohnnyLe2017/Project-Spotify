c44ec488065716109e0601e1034f1024a9c15027
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-08-27 09:02:28 -0400

    Commits

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -13,3 +13,16 @@ if(isset($_POST['loginButton'])) {
 
 }
 ?>
+
+<?php if(isset($_POST['loginButton'])) : ?>
+	<?php //Login button was pressed
+	$username = $_POST['loginUsername'];
+	$password = $_POST['loginPassword'];
+
+	$result = $account->login($username, $password);
+
+	if($result == true) {
+		$_SESSION['userLoggedIn'] = $username;
+		header("Location: index.php");
+	} ?>
+<?php endif; ?>
