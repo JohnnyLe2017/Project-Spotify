0f52d77fdb8839c5806f6e245ab078153b78cce6
Johnny Le <johnnylewebdeveloper@gmail.com>
2020-05-03 09:45:35 -0400

    Updates

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,138 +1,168 @@
 <?php
-class Account {
+class Account
+{
 
-	private $con;
-	private $errorArray;
+    private $con;
+    private $errorArray;
 
-	public function __construct($con) {
-		$this->con = $con;
-		$this->errorArray = array();
-	}
+    public function __construct($con)
+    {
+        $this->con = $con;
+        $this->errorArray = array();
+    }
 
-	public function login($un, $pw) {
+    public function login($username, $password)
+    {
 
-		$pw = password_hash($pw, PASSWORD_BCRYPT, array('cost'=>12));
+        $password = password_hash($password, PASSWORD_BCRYPT, array(
+            'cost' => 12
+        ));
 
-		$query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND password='$pw'");
+        $query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$username' AND password='$password'");
 
-		if(mysqli_num_rows($query) == 1) {
-			return true;
-		}
-		else {
-			array_push($this->errorArray, Constants::$loginFailed);
-			return false;
-		}
+        if (mysqli_num_rows($query) == 1)
+        {
+            return true;
+        }
+        else
+        {
+            array_push($this->errorArray, Constants::$loginFailed);
+            return false;
+        }
 
-	}
+    }
 
-	public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
-		$this->validateUsername($un);
-		$this->validateFirstName($fn);
-		$this->validateLastName($ln);
-		$this->validateEmails($em, $em2);
-		$this->validatePasswords($pw, $pw2);
+    public function register($username, $firstName, $lastName, $email1, $email2, $password, $password2)
+    {
+        $this->validateUsername($username);
+        $this->validateFirstName($firstName);
+        $this->validateLastName($lastName);
+        $this->validateEmails($email1, $email2);
+        $this->validatePasswords($password, $password2);
 
-		if(empty($this->errorArray) == true) {
-			//Insert into db
-			return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
-		}
-		else {
-			return false;
-		}
+        if (empty($this->errorArray) == true)
+        {
+            //Insert into db
+            return $this->insertUserDetails($username, $firstName, $lastName, $email1, $password);
+        }
+        else
+        {
+            return false;
+        }
 
-	}
+    }
 
-	public function getError($error) {
-		if(!in_array($error, $this->errorArray)) {
-			$error = "";
-		}
-		return "<span class='errorMessage'>$error</span>";
-	}
+    public function getError($error)
+    {
+        if (!in_array($error, $this->errorArray))
+        {
+            $error = "";
+        }
+        return "<span class='errorMessage'>$error</span>";
+    }
 
-	private function insertUserDetails($un, $fn, $ln, $em, $pw) {
-		$encryptedPw = password_hash($pw, PASSWORD_BCRYPT, array('cost'=>12));
-		$profilePic = "assets/images/profilepics/image1.png";
-		$date = date("Y-m-d");
+    private function insertUserDetails($username, $firstName, $lastName, $email1, $password)
+    {
+        $$encryptedPassword = password_hash($password, PASSWORD_BCRYPT, array(
+            'cost' => 12
+        ));
+        $profilePic = "assets/images/profilepics/image1.png";
+        $date = date("Y-m-d");
 
-		$result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
+        $result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$username', '$firstName', '$lastName', '$email1', '$$encryptedPassword', '$date', '$profilePic')");
 
-		return $result;
-	}
+        return $result;
+    }
 
-	private function validateUsername($un) {
+    private function validateUsername($username)
+    {
 
-		if(strlen($un) > 25 || strlen($un) < 5) {
-			array_push($this->errorArray, Constants::$usernameCharacters);
-			return;
-		}
+        if (strlen($username) > 25 || strlen($username) < 5)
+        {
+            array_push($this->errorArray, Constants::$usernameCharacters);
+            return;
+        }
 
-		$checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
-		if(mysqli_num_rows($checkUsernameQuery) != 0) {
-			array_push($this->errorArray, Constants::$usernameTaken);
-			return;
-		}
+        $checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$username'");
+        if (mysqli_num_rows($checkUsernameQuery) != 0)
+        {
+            array_push($this->errorArray, Constants::$usernameTaken);
+            return;
+        }
 
-	}
+    }
 
-	private function validateFirstName($fn) {
-		if(strlen($fn) > 25 || strlen($fn) < 2) {
-			array_push($this->errorArray, Constants::$firstNameCharacters);
-			return;
-		}
+    private function validateFirstName($firstName)
+    {
+        if (strlen($firstName) > 25 || strlen($firstName) < 2)
+        {
+            array_push($this->errorArray, Constants::$firstNameCharacters);
+            return;
+        }
 
-		$checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
-			if(mysqli_num_rows($checkUsernameQuery) != 0) {
-				array_push($this->errorArray, Constants::$usernameTaken);
-				return;
-			}
+        $checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$username'");
+        if (mysqli_num_rows($checkUsernameQuery) != 0)
+        {
+            array_push($this->errorArray, Constants::$usernameTaken);
+            return;
+        }
 
-	}
+    }
 
-	private function validateLastName($ln) {
-		if(strlen($ln) > 25 || strlen($ln) < 2) {
-			array_push($this->errorArray, Constants::$lastNameCharacters);
-			return;
-		}
-	}
+    private function validateLastName($lastName)
+    {
+        if (strlen($lastName) > 25 || strlen($lastName) < 2)
+        {
+            array_push($this->errorArray, Constants::$lastNameCharacters);
+            return;
+        }
+    }
 
-	private function validateEmails($em, $em2) {
-		if($em != $em2) {
-			array_push($this->errorArray, Constants::$emailsDoNotMatch);
-			return;
-		}
+    private function validateEmails($em, $email2)
+    {
+        if ($em != $email2)
+        {
+            array_push($this->errorArray, Constants::$emailsDoNotMatch);
+            return;
+        }
 
-		if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-			array_push($this->errorArray, Constants::$emailInvalid);
-			return;
-		}
+        if (!filter_var($em, FILTER_VALIDATE_EMAIL))
+        {
+            array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
 
-		$checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$em'");
-		if(mysqli_num_rows($checkEmailQuery) != 0) {
-			array_push($this->errorArray, Constants::$emailTaken);
-			return;
-		}
+        $checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$em'");
+        if (mysqli_num_rows($checkEmailQuery) != 0)
+        {
+            array_push($this->errorArray, Constants::$emailTaken);
+            return;
+        }
 
-	}
+    }
 
-	private function validatePasswords($pw, $pw2) {
+    private function validatePasswords($password, $password2)
+    {
 
-		if($pw != $pw2) {
-			array_push($this->errorArray, Constants::$passwordsDoNoMatch);
-			return;
-		}
+        if ($password != $password2)
+        {
+            array_push($this->errorArray, Constants::$passwordsDoNoMatch);
+            return;
+        }
 
-		if(preg_match('/[^A-Za-z0-9]/', $pw)) {
-			array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
-			return;
-		}
+        if (preg_match('/[^A-Za-z0-9]/', $password))
+        {
+            array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
+            return;
+        }
 
-		if(strlen($pw) > 30 || strlen($pw) < 5) {
-			array_push($this->errorArray, Constants::$passwordCharacters);
-			return;
-		}
+        if (strlen($password) > 30 || strlen($password) < 5)
+        {
+            array_push($this->errorArray, Constants::$passwordCharacters);
+            return;
+        }
 
-	}
-
+    }
 
 }
 ?>
--- register.php
+++ register.php
@@ -1,19 +1,42 @@
 <?php
-	include("includes/config.php");
-	include("includes/classes/Account.php");
-	include("includes/classes/Constants.php");
+include ("includes/config.php");
+include ("includes/classes/Account.php");
+include ("includes/classes/Constants.php");
 
-	$account = new Account($con);
+$account = new Account($con);
 
-	include("includes/handlers/register-handler.php");
-	include("includes/handlers/login-handler.php");
+include ("includes/handlers/register-handler.php");
+include ("includes/handlers/login-handler.php");
 
-	function getInputValue($name) {
-		if(isset($_POST[$name])) {
-			echo $_POST[$name];
-		}
-	}
+function getInputValue($name)
+{
+    if (isset($_POST[$name]))
+    {
+        echo $_POST[$name];
+    }
+}
+
+if (isset($_POST['registerButton']))
+{
+    echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").hide();
+					$("#registerForm").show();
+				});
+			</script>';
+}
+else
+{
+    echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").show();
+					$("#registerForm").hide();
+				});
+			</script>';
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -33,131 +56,94 @@
 </head>
 
 <body>
-
-	<?php
-
-	if(isset($_POST['registerButton'])) {
-		echo '<script>
-				$(document).ready(function() {
-					$("#loginForm").hide();
-					$("#registerForm").show();
-				});
-			</script>';
-	}
-	else {
-		echo '<script>
-				$(document).ready(function() {
-					$("#loginForm").show();
-					$("#registerForm").hide();
-				});
-			</script>';
-	}
-
-	?>
-		<div id="background">
-				<div id="loginContainer">
-			    <div id="inputContainer">
-			        <form action="register.php" id="loginForm" method="POST">
-			            <h2>Login to your account</h2>
-			            <p>
-											<?php echo $account->getError(Constants::$loginFailed); ?>
-			                <label for="loginUsername">Username</label>
-			                <input type="text" id="loginUsername" name="loginUsername" placeholder="User Name" value="<?php getInputValue('loginUsername') ?>" required>
-			            </p>
-			            <p>
-			                <label for="loginPassword">Password</label>
-			                <input type="password" id="loginPassword" name="loginPassword" placeholder="Enter password" required>
-			            </p>
-			            <button type="submit" name="loginButton">Log In</button>
-			            <div class="hasAccountText">
-			            	<span id="hideLogin">
-			            		Don't have an account yet? Signup here.
-			            	</span>
-			            </div>
-			        </form>
-
-
-
-					<form action="register.php" id="registerForm" method="POST">
-			            <h2>Create your free account</h2>
-
-			            <p>
-											<?php echo $account->getError(Constants::$usernameCharacters); ?>
-											<?php echo $account->getError(Constants::$usernameTaken); ?>
-			                <label for="username">Username</label>
-			                <input type="text" id="username" name="username" placeholder="User Name" value="<?php getInputValue('username') ?>" required>
-			            </p>
-
-			            <p>
-											<?php echo $account->getError(Constants::$firstNameCharacters); ?>
-			                <label for="firstName">First Name</label>
-			                <input type="text" id="firstName" name="firstName" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
-			            </p>
-
-			            <p>
-											<?php echo $account->getError(Constants::$lastNameCharacters); ?>
-			                <label for="lastName">Last Name</label>
-			                <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="<?php getInputValue('lastName') ?>" required>
-			            </p>
-
-			            <p>
-										<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-										<?php echo $account->getError(Constants::$emailInvalid); ?>
-										<?php echo $account->getError(Constants::$emailTaken); ?>
-			                <label for="email">Email</label>
-			                <input type="email" id="email" name="email" placeholder="Email" value="<?php getInputValue('email') ?>" required>
-			            </p>
-
-			            <p>
-			                <label for="email2">Confirm email</label>
-			                <input type="email" id="email2" name="email2" placeholder="Confirm email" value="<?php getInputValue('email2') ?>" required>
-			            </p>
-
-			            <p>
-										<?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
-										<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-										<?php echo $account->getError(Constants::$passwordCharacters); ?>
-			                <label for="password">Password</label>
-			                <input type="password" id="password" name="password" placeholder="Enter password" required>
-			            </p>
-
-			             <p>
-			                <label for="password2">Confirm password</label>
-			                <input type="password" id="password2" name="password2" placeholder="Enter password" required>
-			            </p>
-
-			            <button type="submit" name="registerButton">SIGN UP</button>
-
-			            <div class="hasAccountText">
-			            	<span id="hideRegister">
-			            		Already have an account? Log in here.
-			            	</span>
-			            </div>
-
-			        </form>
-
-			    </div>
-
-				    <div id="loginText">
-					<h1>Music for everyone.</h1>
-					<h2>What's on Spotify?</h2>
-					<h3>Music</h3>
-					<p class="music">
-						There are millions of songs on Spotify. Play your favorites, discover new tracks, and build the perfect collection.
-					</p>
-					<h3>Playlists</h3>
-					<p class="music">
-						You’ll find readymade playlists to match your mood, put together by music fans and experts.
-					</p>
-					<h3>New Releases</h3>
-					<p class="music">
-						Hear this week’s latest singles and albums, and check out what’s hot in the Top 50.
-					</p>
-
-			</div>
-		</div>
-		<script src="js/register.js"></script>
-    <script src="js/script.js"></script>
+    <div id="background">
+        <div id="loginContainer">
+            <div id="inputContainer">
+                <form action="register.php" id="loginForm" method="POST">
+                    <h2>Login to your account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$loginFailed); ?>
+                        <label for="loginUsername">Username</label>
+                        <input type="text" id="loginUsername" name="loginUsername" placeholder="User Name" value="<?php getInputValue('loginUsername') ?>" required>
+                    </p>
+                    <p>
+                        <label for="loginPassword">Password</label>
+                        <input type="password" id="loginPassword" name="loginPassword" placeholder="Enter password" required>
+                    </p>
+                    <button type="submit" name="loginButton">Log In</button>
+                    <div class="hasAccountText">
+                        <span id="hideLogin">
+                            Don't have an account yet? Signup here.
+                        </span>
+                    </div>
+                </form>
+                <form action="register.php" id="registerForm" method="POST">
+                    <h2>Create your free account</h2>
+                    <p>
+                        <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                        <?php echo $account->getError(Constants::$usernameTaken); ?>
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username" placeholder="User Name" value="<?php getInputValue('username') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                        <label for="firstName">First Name</label>
+                        <input type="text" id="firstName" name="firstName" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+                        <label for="lastName">Last Name</label>
+                        <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="<?php getInputValue('lastName') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+                        <?php echo $account->getError(Constants::$emailInvalid); ?>
+                        <?php echo $account->getError(Constants::$emailTaken); ?>
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Email" value="<?php getInputValue('email') ?>" required>
+                    </p>
+                    <p>
+                        <label for="email2">Confirm email</label>
+                        <input type="email" id="email2" name="email2" placeholder="Confirm email" value="<?php getInputValue('email2') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                        <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                        <?php echo $account->getError(Constants::$passwordCharacters); ?>
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="Enter password" required>
+                    </p>
+                    <p>
+                        <label for="password2">Confirm password</label>
+                        <input type="password" id="password2" name="password2" placeholder="Enter password" required>
+                    </p>
+                    <button type="submit" name="registerButton">SIGN UP</button>
+                    <div class="hasAccountText">
+                        <span id="hideRegister">
+                            Already have an account? Log in here.
+                        </span>
+                    </div>
+                </form>
+            </div>
+            <div id="loginText">
+                <h1>Music for everyone.</h1>
+                <h2>What's on Spotify?</h2>
+                <h3>Music</h3>
+                <p class="music">
+                    There are millions of songs on Spotify. Play your favorites, discover new tracks, and build the perfect collection.
+                </p>
+                <h3>Playlists</h3>
+                <p class="music">
+                    You’ll find readymade playlists to match your mood, put together by music fans and experts.
+                </p>
+                <h3>New Releases</h3>
+                <p class="music">
+                    Hear this week’s latest singles and albums, and check out what’s hot in the Top 50.
+                </p>
+            </div>
+        </div>
+        <script src="js/register.js"></script>
+        <script src="js/script.js"></script>
 </body>
 
 </html>
